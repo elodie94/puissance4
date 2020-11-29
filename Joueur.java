b8159b2be19e8c515b6ccfd6c48a8ef80e0ee3a6
Elodie Jolo <elodie.jolo@etu.u-pec.fr>
2020-11-29 17:38:50 +0100

    Téléverser un nouveau fichier

---
+++ Joueur.java
@@ -0,0 +1,32 @@
+
+/**
+ * Classe représentant un joueur (humain ou IA)
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Joueur
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private boolean type;
+    private String nom;
+    private int nb_pion; //pour détecter une fin de partie
+    private String jeton;
+
+    /**
+     * Constructeur d'objets de classe Joueur
+     */
+    public Joueur()
+    {
+        // initialisation des variables d'instance
+        
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    
+}
