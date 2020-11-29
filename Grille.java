87e4431647125dd0ec80b5870794df1ea6f1e5da
Elodie Jolo <elodie.jolo@etu.u-pec.fr>
2020-11-29 17:38:29 +0100

    Téléverser un nouveau fichier

---
+++ Grille.java
@@ -0,0 +1,32 @@
+
+/**
+ * Classe représentant la grille du puissance 4
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Grille
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private final int colonne, ligne;
+    private Case[][] grille;
+    private int nb_pions;
+
+    /**
+     * Constructeur d'objets de classe Grille
+     */
+    public Grille(int c,int l)
+    {
+        // initialisation des variables d'instance
+        this.colonne=7+2;
+        this.ligne=6+2;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    
+}
