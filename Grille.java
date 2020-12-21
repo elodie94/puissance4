53d5cb17f8c07fe8a0f51955c4f33818faab979f
Sarah Feqqoussi <sarah.feqqoussi@etu.u-pec.fr>
2020-12-21 15:14:13 +0100

    Update Grille.java

--- Grille.java
+++ Grille.java
@@ -15,18 +15,61 @@ public class Grille
     /**
      * Constructeur d'objets de classe Grille
      */
-    public Grille(int c,int l)
-    {
-        // initialisation des variables d'instance
-        this.colonne=7+2;
-        this.ligne=6+2;
-    }
-
-    /**
-     * Un exemple de méthode - remplacez ce commentaire par le vôtre
-     *
-     * @param  y   le paramètre de la méthode
-     * @return     la somme de x et de y
-     */
+    // public Grille(int c,int l)
+    // {
+        // // initialisation des variables d'instance
+        // this.colonne=7+2;
+        // this.ligne=6+2;
+    // }
     
+ 
+    public Grille(int [][] grille, int colonne, int ligne)
+    {
+        this.colonne = colonne;
+        this.ligne = ligne;
+    }
+    
+    /**
+     * Cette methode nous permet de savoir si une colonne est rempli ou non
+     *
+     * @param  [][]grille   le paramètre, le tableau de case
+     * @param  colonne      le numéro de colonne dont on souhaite savoir si elle est rempli ou pas 
+     * @return              true si la colonne est rempli, false sinon
+     */
+    public boolean verifier_colonne (Case [][]grille, int colonne){
+        if (colonne > 7) return true;
+        else { 
+            for (int i = 0; i<6; i++){
+                if (grille[colonne][i] == Case.VIDE)return false; 
+            }
+        return true;
+       }
+   }
+   
+   
+       /**
+     * Cette methode nous permet de savoir si la grille est remplie ou pas 
+     *
+     * @param  [][]grille   la grille dont on veut savoir si elle est pleine ou non
+     * @return              true si la grille est rempli, false sinon
+     */
+   public boolean grille_pleine (Case [][] grille){
+       for (int i = 0; i<7 ; i++){  // Colonne 
+           for (int j = 0; j<6; j++){// Lignes
+               if (grille[i][j] != Case.VIDE) return false;
+            }
+        }
+        return true;
+    }
+   
+           /**
+     * Cette methode nous permet de détecter s'il y'a eu une victoire
+     *
+     * @param  [][]grille   la grille dont on veut savoir si elle est pleine ou non
+     * @return              0 si il y'a match nulle, 1 si le joueur aux pions X a gagné et 2 si je joueur aux pions O a gagné
+     */
+  // public int detection_victoire (Case[][]grille){
+       
+   
+    // }
 }
