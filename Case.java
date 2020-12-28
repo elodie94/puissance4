a89b8c249aaf3172f53255f11ff4746ce504f7f1
Sarah Feqqoussi <sarah.feqqoussi@etu.u-pec.fr>
2020-12-28 22:04:31 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -2,7 +2,7 @@
  * Enumeration Case - Représentation de la case dans le jeu avec vide pour une case vide, X pour représenter le jeton d'un joueur et O pour représenter le jeton du deuxième joueur
  *
  * @author JOLO Elodie FEQQOUSSI SARAH
- * @version (numéro de version ou date)
+ * @version 28/12/2020
  */
 public enum Case
 {
