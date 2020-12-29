d0eddf81821efa7325b7aacd2e070aef4c58d40f
Elodie Jolo <elodie.jolo@etu.u-pec.fr>
2020-12-29 18:21:06 +0100

    Téléverser un nouveau fichier

---
+++ PartieH_H.java
@@ -0,0 +1,24 @@
+
+/**
+ * Décrivez votre classe PartieH_H ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class PartieH_H
+{
+    private Joueur h1;
+    private Joueur h2;
+
+    /**
+     * Constructeur d'objets de classe PartieH_H
+     */
+    public PartieH_H(Joueur h1,Joueur h2)
+    {
+        // initialisation des variables d'instance
+        this.h1=h1;
+        this.h2=h2;
+    }
+
+    
+}
