b7396e8c8f0d38dbf47c77348b4b37348786dc7e
Elodie Jolo <elodie.jolo@etu.u-pec.fr>
2020-12-29 18:15:40 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,32 +1,52 @@
-
+import java.util.Scanner;
 /**
  * Classe représentant un joueur (humain ou IA)
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author FEQQOUSSI Sarah JOLO Elodie
+ * @version 29/12/2020
  */
 public class Joueur
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
     private boolean type;
     private String nom;
-    private int nb_pion; //pour détecter une fin de partie
-    private String jeton;
+    //private int nb_pion; //pour détecter une fin de partie
+    private Case jeton;
 
     /**
      * Constructeur d'objets de classe Joueur
+     * 
+     * @param type si c'est un humain ou une IA
+     * @param nom du joueur
+     * @param jeton du joueur
      */
-    public Joueur()
+    public Joueur(boolean type,String nom,Case jeton)
     {
         // initialisation des variables d'instance
-        
+        this.type=type;
+        this.nom=nom;
+        this.jeton=jeton;
+    }
+    
+    public Joueur()
+    {
+        this(true,null,Case.VIDE);
+    }
+    
+    public boolean getType()
+    {
+        return this.type;
     }
 
-    /**
-     * Un exemple de méthode - remplacez ce commentaire par le vôtre
-     *
-     * @param  y   le paramètre de la méthode
-     * @return     la somme de x et de y
-     */
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+    public Case getJeton()
+    {
+        return this.jeton;
+    }
+    
     
 }
