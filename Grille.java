06b372cfa0526100987b558f912a33bec10657b4
Sarah Feqqoussi <sarah.feqqoussi@etu.u-pec.fr>
2021-01-05 13:26:29 +0100

    Update Grille.java

--- Grille.java
+++ Grille.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 /**
  * Classe représentant la grille du puissance 4
  *
@@ -7,47 +6,98 @@ import java.util.ArrayList;
  */
 public class Grille
 {
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private final int colonne, ligne;
+    private Case[][] grille; //ou ArrayList<Colonne> grille;
     private int nb_pions;
 
     /**
      * Constructeur d'objets de classe Grille
      */
-    public Grille(int nb_pions){
-        this.nb_pions = nb_pions;
+    // public Grille(int c,int l)
+    // {
+        // // initialisation des variables d'instance
+        // this.colonne=c+2;
+        // this.ligne=l+2;
+    // }
+    
+ 
+    public Grille(int colonne, int ligne)
+    {
+        this.colonne = colonne;
+        this.ligne = ligne;
+        grille=new Case[colonne][ligne];
         
+        //initialiser la grille vide
+        for(int i=0;i<colonne;i++){
+            for(int j=0;j<ligne;j++){
+                grille[i][j]=Case.VIDE;
+            }
+        }
     }
     
-          
+    /**
+     * Cette methode nous permet de savoir si une colonne est rempli ou non
+     *
+     * @param  [][]grille   le paramètre, le tableau de case
+     * @param  colonne      le numéro de colonne dont on souhaite savoir si elle est rempli ou pas 
+     * @return              true si la colonne est rempli, false sinon
+     */
+    public boolean verifier_colonne (Case [][]grille, int colonne){
+        if (colonne > 7) return true;
+        else { 
+            for (int i = 0; i<6; i++){
+                if (grille[colonne][i] == Case.VIDE)return false; 
+            }
+        return true;
+       }
+   }
+   
+   
        /**
      * Cette methode nous permet de savoir si la grille est remplie ou pas 
      *
-     * @param  liste_colonne  la liste de colonne
-     * @param  liste_case     la liste de case
+     * @param  [][]grille   la grille dont on veut savoir si elle est pleine ou non
      * @return              true si la grille est rempli, false sinon
      */
-    
-   public boolean grille_pleine (ArrayList liste_colonne, ArrayList liste_case)
-   {
-       if (liste_colonne.size() > 7 || liste_case.size() > 35) return true;
-       else return false;
-     }
+   public boolean grille_pleine (Case [][] grille){
+       for (int i = 0; i<7 /*colonne*/; i++){  // Colonne 
+           for (int j = 0; j<6 /*ligne*/; j++){// Lignes
+               if (grille[i][j] != Case.VIDE) return false;
+            }
+        }
+        return true;
+    }
    
+           /**
+     * Cette methode nous permet de détecter s'il y'a eu une victoire
+     *
+     * @param  [][]grille   la grille dont on veut savoir si elle est pleine ou non
+     * @return              0 si il y'a match nulle, 1 si le joueur aux pions X a gagné et 2 si je joueur aux pions O a gagné
+     */
+  // public int detection_victoire (Case[][]grille){
+       
    
+    // }
 
     /**
-     * Cette methode nous permet d'afficher la grille
+     * afficher la grille
      * 
      */
-    public void afficher_grille(ArrayList liste_colonne, ArrayList liste_case)
+    public void afficher_grille()
     {
-        // Afficher la première ligne
-        for(int i = 0; i < liste_colonne.size(); i++){
-         System.out.println(liste_colonne.get(i));
-         
-         //Afficher la colonne en entière à partir de l'index récupéré dans la boucle précédente 
-         for(int j=1; j<liste_case.size(); j++){ 
-             // System.out.println (liste_colonne.get(i).liste_case.get(j)); Ca ne compile pas
+        //pour avoir les numéros de colonnes
+        for(int nc=1;nc<colonne;nc++){
+            System.out.print(nc);
+        }
+        System.out.println("");
+        //pour afficher le contenu de la grille
+        for(int i=0;i<ligne;i++){
+            System.out.print("|");
+            for(int j=0;j<colonne;j++){
+                System.out.print(grille[i][j]+"|");
             }
+            System.out.println("");
         }
     }
 }
