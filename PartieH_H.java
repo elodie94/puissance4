cee84ab0abdec4beb4f2d650a8adc5bb66e79d41
Sarah Feqqoussi <sarah.feqqoussi@etu.u-pec.fr>
2021-01-09 20:31:45 +0100

    Update PartieH_H.java

--- PartieH_H.java
+++ PartieH_H.java
@@ -1,33 +1,28 @@
 import java.util.Scanner;
 import java.io.*;
 /**
- * Classe pour lancer une partie humain contre humain
+ * classe pour lancer une partie humain contre IA
  *
  * @author FEQQOUSSI Sarah JOLO Elodie
- *
+ * 
  */
-public class PartieH_H
+public class PartieH_IA
 {
 
     /**
-     * Constructeur d'objets de classe PartieH_H
+     * Constructeur d'objets de classe PartieH_IA
      */
-    public PartieH_H()
+    public PartieH_IA()
     {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Saisir le nom du premier joueur:");
+        System.out.println("Saisir le nom du joueur:");
         String nomj1=scanner.nextLine();
         Jeu.saut_ligne();
 
-        System.out.println("Saisissez le nom du deuxième joueur :");
-        String nomj2=scanner.nextLine();
-        Jeu.saut_ligne();
-
         Joueur j1=new Joueur(true,nomj1,Case.X);
-        Joueur j2=new Joueur(true,nomj2,Case.O);
+        Joueur j2=new Joueur(false,"bot",Case.O);
 
         //choisir la taille de la grille ou une sauvegarde
-
         String[][] g;
         File f = new File("D:\\sauvegarde.txt");
 
@@ -35,6 +30,7 @@ public class PartieH_H
         {
             System.out.println("Voulez-vous lancer une sauvegarde ? (reponse oui ou non)");
             String reponse=scanner.nextLine();
+            Jeu.saut_ligne();
 
             //lancement sauvegarde si reponse est oui
             if (reponse.equals("Oui") || reponse.equals("OUI") || reponse.equals("oui")){
@@ -44,15 +40,10 @@ public class PartieH_H
                 Jeu.saut_ligne();
 
                 Joueur jcour;
-
                 if(j1.getJeton().getRep().equals(gr.prochain_pion (g).getRep())) jcour=new Joueur(j1);
                 else jcour=new Joueur(j2);
 
                 boolean arret_jeu=false;
-
-                System.out.println("Si vous voulez sauvegarder la partie, tapez 0 lors de la sélection de colonne");
-                Jeu.saut_ligne();
-
                 while(arret_jeu==false){
 
                     if(jcour.equals(j1)){
@@ -61,35 +52,53 @@ public class PartieH_H
                         jcour=j1;
                     }
 
-                    System.out.println(jcour.getNom()+", saisissez le numéro de la colonne où vous voulez jouer : ");
+                    if (jcour.getType() == true){
+                        System.out.println(jcour.getNom()+", saisissez le numéro de la colonne où vous voulez jouer : ");
 
-                    int coloj=scanner.nextInt();
-                    Jeu.saut_ligne();
-
-                    if(coloj==0){
-                        Jeu.sauvegarder("D:\\sauvegarde.txt",gr.getGrille());
-                        System.exit(0);
-                    }
-
-                    while(coloj<1 && coloj>gr.getColonne()){
-                        System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne());
-                        coloj=scanner.nextInt();
+                        int coloj=scanner.nextInt();
                         Jeu.saut_ligne();
-                    }
 
-                    while(gr.ajout_pion(jcour, gr.getGrille(),coloj) == false){
-                        System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne()+" sauf "+coloj);
-                        coloj=scanner.nextInt();
+                        if(coloj==0){
+                            Jeu.sauvegarder("D:\\sauvegarde.txt",gr.getGrille());
+                            System.exit(0);
+                        }
+
+                        while(coloj<1 && coloj>gr.getColonne()){
+                            System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne());
+                            coloj=scanner.nextInt();
+                            Jeu.saut_ligne();
+                        }
+
+                        while(gr.ajout_pion(jcour, gr.getGrille(),coloj) == false){
+                            System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne()+" sauf "+coloj);
+                            coloj=scanner.nextInt();
+                            Jeu.saut_ligne();
+                        }
+
+                        gr.afficher_grille();
                         Jeu.saut_ligne();
+
+                        if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){
+                            arret_jeu=true;
+                        }
+
+                        
                     }
+                    else{
+                        try { 
+                            Thread.sleep(3000); 
+                        } catch (InterruptedException e) { 
+                            e.printStackTrace(); 
+                        } 
+                        
+                        int coloj = gr.ajout_pion_IA (jcour, gr.getGrille());
+                        gr.afficher_grille();
+                        Jeu.saut_ligne();
 
-                    gr.afficher_grille();
-                    Jeu.saut_ligne();
-
-                    if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){
-                        arret_jeu=true;
+                        if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){ 
+                            arret_jeu=true;
+                        }
                     }
-
                 }
             }else{
                 int tc=Jeu.taille_colonne();
@@ -101,9 +110,6 @@ public class PartieH_H
                 boolean arret_jeu=false;
                 Joueur jcour=new Joueur(j1);
 
-                System.out.println("Si vous voulez sauvegarder la partie, tapez 0 lors de la sélection de colonne");
-                Jeu.saut_ligne();
-
                 while(arret_jeu==false){
 
                     if(jcour.equals(j1)){
@@ -112,6 +118,75 @@ public class PartieH_H
                         jcour=j1;
                     }
 
+                    if (jcour.getType() == true){
+                        System.out.println(jcour.getNom()+", saisissez le numéro de la colonne où vous voulez jouer : ");
+
+                        int coloj=scanner.nextInt();
+                        Jeu.saut_ligne();
+
+                        if(coloj==0){
+                            Jeu.sauvegarder("D:\\sauvegarde.txt",gr.getGrille());
+                            System.exit(0);
+                        }
+
+                        while(coloj<1 && coloj>gr.getColonne()){
+                            System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne());
+                            coloj=scanner.nextInt();
+                            Jeu.saut_ligne();
+                        }
+
+                        while(gr.ajout_pion(jcour, gr.getGrille(),coloj) == false){
+                            System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne()+" sauf "+coloj);
+                            coloj=scanner.nextInt();
+                            Jeu.saut_ligne();
+                        }
+
+                        gr.afficher_grille();
+                        Jeu.saut_ligne();
+
+                        if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){
+                            arret_jeu=true;
+                        }
+
+                        //deuxieme cas
+                    }
+                    else{
+
+                        try { 
+                            Thread.sleep(3000); 
+                        } catch (InterruptedException e) { 
+                            e.printStackTrace(); 
+                        } 
+
+                        int coloj = gr.ajout_pion_IA (jcour, gr.getGrille());
+                        gr.afficher_grille();
+                        Jeu.saut_ligne();
+
+                        if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){ 
+                            arret_jeu=true;
+                        }
+                    }
+                }
+            }
+        }else{
+            int tc=Jeu.taille_colonne();
+            int tl=Jeu.taille_ligne();
+            Grille gr=new Grille(tc,tl);
+            gr.afficher_grille();
+            Jeu.saut_ligne();
+
+            boolean arret_jeu=false;
+            Joueur jcour=new Joueur(j1);
+
+            while(arret_jeu==false){
+
+                if(jcour.equals(j1)){
+                    jcour=j2;
+                }else{
+                    jcour=j1;
+                }
+
+                if (jcour.getType() == true){
                     System.out.println(jcour.getNom()+", saisissez le numéro de la colonne où vous voulez jouer : ");
 
                     int coloj=scanner.nextInt();
@@ -140,57 +215,24 @@ public class PartieH_H
                     if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){
                         arret_jeu=true;
                     }
+
+                    //deuxieme cas
                 }
-            }
-        }else{
+                else{
+                    try { 
+                        Thread.sleep(800); 
+                    } catch (InterruptedException e) { 
+                        e.printStackTrace(); 
+                    } 
 
-            int tc=Jeu.taille_colonne();
-            int tl=Jeu.taille_ligne();
-            Grille gr=new Grille(tc,tl);
-            gr.afficher_grille();
-            Jeu.saut_ligne();
-
-            boolean arret_jeu=false;
-            Joueur jcour=new Joueur(j1);
-
-            System.out.println("Si vous voulez sauvegarder la partie, tapez 0 lors de la sélection de colonne");
-            Jeu.saut_ligne();
-
-            while(arret_jeu==false){
-
-                if(jcour.equals(j1)){
-                    jcour=j2;
-                }else{
-                    jcour=j1;
-                }
-
-                System.out.println(jcour.getNom()+", saisissez le numéro de la colonne où vous voulez jouer : ");
-
-                int coloj=scanner.nextInt();
-                Jeu.saut_ligne();
-
-                if(coloj==0){
-                    Jeu.sauvegarder("D:\\sauvegarde.txt",gr.getGrille());
-                    System.exit(0);
-                }
-
-                while(coloj<1 && coloj>gr.getColonne()){
-                    System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne());
-                    coloj=scanner.nextInt();
+                    int coloj = gr.ajout_pion_IA (jcour, gr.getGrille());
+                    gr.afficher_grille();
                     Jeu.saut_ligne();
-                }
 
-                while(gr.ajout_pion(jcour, gr.getGrille(),coloj) == false){
-                    System.out.println("Veuillez saisir un numéro de colonne compris entre 1 et "+gr.getColonne()+" sauf "+coloj);
-                    coloj=scanner.nextInt();
-                    Jeu.saut_ligne();
-                }
+                    if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){ 
+                        arret_jeu=true;
+                    }
 
-                gr.afficher_grille();
-                Jeu.saut_ligne();
-
-                if(gr.detection_victoire(gr.getGrille(),coloj,jcour)==true){
-                    arret_jeu=true;
                 }
             }
         }
