10d82099db9ff5b34f45cfe460b5398003149c56
Sarah Feqqoussi <sarah.feqqoussi@etu.u-pec.fr>
2021-01-09 13:51:26 +0100

    Update PartieH_H.java

--- PartieH_H.java
+++ PartieH_H.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+
 import java.io.*;
 /**
  * Classe représentant une partie humain contre humain
  *
- * @author JOLO ELodie, FEQQOUSSI Sarah
+ * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
-public class PartieH_H
+public class CreerPartie
 {
     private Joueur h1;
     private Joueur h2;
@@ -14,13 +14,12 @@ public class PartieH_H
     /**
      * Constructeur d'objets de classe PartieH_H
      */
-    public PartieH_H(Joueur h1,Joueur h2)
+    public CreerPartie(Joueur h1,Joueur h2)
     {
         // initialisation des variables d'instance
         this.h1=h1;
         this.h2=h2;
     }
-      
 
     
 }
