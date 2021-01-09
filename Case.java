7e6010ce368d531079798e494a25990f11d7cff5
Elodie Jolo <elodie.jolo@etu.u-pec.fr>
2021-01-09 19:39:00 +0100

    Replace Case.java

--- Case.java
+++ Case.java
@@ -1,27 +1,28 @@
+
 /**
  * Enumeration Case - Représentation de la case dans le jeu avec vide pour une case vide, X pour représenter le jeton d'un joueur et O pour représenter le jeton du deuxième joueur
  *
- * @author JOLO Elodie FEQQOUSSI SARAH
- * @version 28/12/2020
+ * @author (votre nom)
+ * @version (numéro de version ou date)
  */
 public enum Case
 {
-    VIDE(' '), X('X'), O('O');
+    VIDE(" "),X("X"),O("O");
     
     //représentation de la case
-    private char rep;
+    private String rep;
     
-    private Case(char rep)
+    private Case(String rep)
     {
         this.rep=rep;
     }
     
     /**
-     * constructeur pour récupérer le représentation de la case
+     * constructeur pour récupérer la représentation de la case
      * 
      * @return rep représentation de la case
      */
-    public char getRep()
+    public String getRep()
     {
         return this.rep;
     }
